62c4f67c5507e4b2d5c20789649f50b87e25f40f
Archana Singh <archana21@navgurukul.org>
2023-11-03 15:15:47 +0530

    Form_ready

--- client_task/src/component/Form.js
+++ client_task/src/component/Form.js
@@ -1,129 +1,180 @@
+// import React, { useState } from "react";
+// import Header from "./Header";
+
+// function PropertyForm() {
+//   const [formData, setFormData] = useState({
+//     address: "",
+//     geoLocation: "",
+//     configuration: "",
+//     amenities: "",
+//     availability: "",
+//     rent: "",
+//     maintenance: "",
+//     deposit: "",
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // You can process or display the formData here
+//     console.log("Form Data:", formData);
+//   };
+
+//   return (
+//     <>
+//       <Header />
+//       <div className="container">
+//         <h2>Property Details</h2>
+
+//         <form onSubmit={handleSubmit}>
+//           Address:-
+//           <input className="form"
+//             type="text"
+//             id="address"
+//             name="address"
+//             value={formData.address}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           GeoLocation:-
+//           <input className="form"
+//             type="text"
+//             id="geoLocation"
+//             name="geoLocation"
+//             value={formData.geoLocation}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           Configuration:-
+//           <input className="form"
+//             type="text"
+//             id="configuration"
+//             name="configuration"
+//             value={formData.configuration}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           Amenities:-
+//           <input className="form"
+//             type="text"
+//             id="amenities"
+//             name="amenities"
+//             value={formData.amenities}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           Availability:-
+//           <input className="form"
+//             type="text"
+//             id="availability"
+//             name="availability"
+//             value={formData.availability}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           Rent Price:-
+//           <input className="form"
+//             type="number"
+//             id="rent"
+//             name="rent"
+//             value={formData.rent}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           Maintenance Price:-
+//           <input className="form"
+//             type="number"
+//             id="maintenance"
+//             name="maintenance"
+//             value={formData.maintenance}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           Deposit Price:-
+//           <input className="form"
+//             type="number"
+//             id="deposit"
+//             name="deposit"
+//             value={formData.deposit}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           <button type="submit">Submit</button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default PropertyForm;
+
+
+
+
+
+
+
+
 import React, { useState } from "react";
-import Header from "./Header";
 
-function PropertyForm() {
-  const [formData, setFormData] = useState({
-    address: "",
-    geoLocation: "",
-    configuration: "",
-    amenities: "",
-    availability: "",
-    rent: "",
-    maintenance: "",
-    deposit: "",
-  });
+function PhotoUpload() {
+const [selectedFile, setSelectedFile] = useState(null);
+const [images, setImages] = useState([]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+const handleFileChange = (event) => {
+setSelectedFile(event.target.files[0]);
+};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can process or display the formData here
-    console.log("Form Data:", formData);
-  };
+const handleUpload = () => {
+const formData = new FormData();
+formData.append("photo", selectedFile);
 
-  return (
-    <>
-      <Header />
-      <div className="container">
-        <h2>Property Details</h2>
-
-        <form onSubmit={handleSubmit}>
-          Address:-
-          <input className="form"
-            type="text"
-            id="address"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            required
-          />
-
-          GeoLocation:-
-          <input className="form"
-            type="text"
-            id="geoLocation"
-            name="geoLocation"
-            value={formData.geoLocation}
-            onChange={handleChange}
-            required
-          />
-
-          Configuration:-
-          <input className="form"
-            type="text"
-            id="configuration"
-            name="configuration"
-            value={formData.configuration}
-            onChange={handleChange}
-            required
-          />
-
-          Amenities:-
-          <input className="form"
-            type="text"
-            id="amenities"
-            name="amenities"
-            value={formData.amenities}
-            onChange={handleChange}
-            required
-          />
-
-          Availability:-
-          <input className="form"
-            type="text"
-            id="availability"
-            name="availability"
-            value={formData.availability}
-            onChange={handleChange}
-            required
-          />
-
-          Rent Price:-
-          <input className="form"
-            type="number"
-            id="rent"
-            name="rent"
-            value={formData.rent}
-            onChange={handleChange}
-            required
-          />
-
-          Maintenance Price:-
-          <input className="form"
-            type="number"
-            id="maintenance"
-            name="maintenance"
-            value={formData.maintenance}
-            onChange={handleChange}
-            required
-          />
-
-          Deposit Price:-
-          <input className="form"
-            type="number"
-            id="deposit"
-            name="deposit"
-            value={formData.deposit}
-            onChange={handleChange}
-            required
-          />
-
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    </>
-  );
+fetch("http://localhost:3000/upload", {
+method: "POST",
+body: formData,
+})
+.then((response) => response.json())
+.then((data) => {
+console.log("Upload successful:", data);
+setSelectedFile(null);
+alert("Upload successful");
+})
+.catch((error) => {
+console.error("Error uploading photo:", error);
+});
+};
+const getImages = async () => {
+fetch("http://localhost:3000/all-photo", { method: "GET" })
+.then((response) => response.json())
+.then((res) => {
+console.log("Upload data:", res);
+setImages(res.data);
+});
+};
+console.log(selectedFile);
+return (
+<div>
+<h1>Photo Upload</h1>
+<input type="text" onChange={handleFileChange} defaultValue={""} />
+<button onClick={handleUpload}>Upload</button>
+<button onClick={getImages}>Get all images</button>
+{images.map((e) => (
+<img src={e.url} alt="all" />
+))}
+</div>
+);
 }
 
-export default PropertyForm;
-
-
-
-
-
-
-
-
+export default PhotoUpload;
